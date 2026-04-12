bad624f13d47d0d6af513a08e6f71c77e1440b44
NoahSSJ <qwerasdfk902@gmail.com>
2026-04-12 21:38:20 +0800

    4.12提交

--- 签名逆向/信用中国√/main.py
+++ 签名逆向/信用中国√/main.py
@@ -1,25 +1,26 @@
 import execjs
 import requests
 
+# 请求头
 headers = {
     'sec-ch-ua-platform': '"Windows"',
     'Referer': 'https://credit.hd.gov.cn/xyxxgs/',
     'sec-ch-ua': '"Chromium";v="146", "Not-A.Brand";v="24", "Microsoft Edge";v="146"',
     'sec-ch-ua-mobile': '?0',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36 Edg/146.0.0.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
     'Accept': 'application/json, text/plain, */*',
     'x-gateway-body': 'blob',
     'C-GATEWAY-QUERY-ENCRYPT': '1',
 }
 
+# 1. 加载你的参数生成 JS
 with open('a.js', encoding='utf-8') as f:
-    ctx = execjs.compile(f.read())
+    ctx_params = execjs.compile(f.read())
+params = ctx_params.call('get_Params')
 
-params = ctx.call('get_Params')
-
+# 2. 获取加密二进制数据
 response = requests.get(
     'https://credit.hd.gov.cn/zx_website/website/sgs/xzxkfr',
     headers=headers,
     params=params
 )
-
